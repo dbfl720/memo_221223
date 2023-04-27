326bf1f1a5f2dbc70f73784a2a4cf490e317bae4
YURI HONG <dbfl720@gmail.com>
2023-04-27 21:43:48 +0900

    memo -  마지막 수정

--- src/main/java/com/memo/config/WebMvcConfig.java
+++ src/main/java/com/memo/config/WebMvcConfig.java
@@ -1,13 +1,20 @@
 package com.memo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.memo.Interceptor.PermissionInterceptor;
 import com.memo.common.FileManagerService;
 
 @Configuration  //  설정을 위한 spring bean
 public class WebMvcConfig implements WebMvcConfigurer {
+	
+	
+	@Autowired
+	private PermissionInterceptor interceptor;
 	
 	// 서버에 업로드 된 이미지와 웹 이미지 주소와의 맵핑 설정
 	@Override
@@ -15,8 +22,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry
 		.addResourceHandler("/images/**") // 웹 이미지 주소  http://localhost/images/yuri_1681728082266/amsterdam-7917858_1280.jpg
 		.addResourceLocations("file://" + FileManagerService.FILE_UPLOAD_PATH);  // 실제 파일 위치  - mac은 // 슬래시 2개 . - 개수 틀리면 404 // **과 연동 되면서 되는 것임.
-		
+	}
 	
 	
+	// interceptor 설정 추가
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+		.addInterceptor(interceptor)
+		.addPathPatterns("/**")  //  /** - 자식들까지(아래 디렉토리까지)  전부 확인
+		.excludePathPatterns("/favicon.ico", "/error", "/static/**", "/user/sign_out")		
+		; 
 	}
 }
